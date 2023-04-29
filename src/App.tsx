415e1d0402634702a094028d817f4329648750ef
Alexandr Tokarev <46864005+AlexandrTokarev@users.noreply.github.com>
2023-04-29 21:28:04 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import classes from './App.module.scss';
+
+function App() {
+  return (
+    <main className={classes.root}>
+      <header className={classes.header}>
+
+      </header>
+    </main>
+  )
+}
+
+export default App
